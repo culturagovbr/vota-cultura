40c39dff5a41fed480c427368218758c2dfad67f
vinicius.feitosa <viniciusfesil@gmail.com>
2019-07-08 15:34:48 -0300

    [ADD] Criação de Model e Factory para Perfil
    Co-authored-by: Jorge Luiz <jorge.junior@terceirizado.cidadania.gov.br>

---
+++ api/database/factories/PerfilFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+
+$factory->define(\App\Modules\Conta\Model\Perfil::class, function (Faker $faker) {
+    return [
+        'no_perfil' => 'usuario',
+        'ds_perfil' => 'Perfil padrão para todos os usuário do sitema',
+        'st_ativo' => true,
+    ];
+});
