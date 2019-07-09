817bd4e942b56d68bda0929c156cb4f5fd4a6b71
vinicius.feitosa <viniciusfesil@gmail.com>
2019-07-09 15:37:35 -0300

    [ADD] Criação de template Foo para criação de módulos

---
+++ api/app/Modules/Foo/Providers/AutenticacaoServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Modules\Foo\Providers;
+
+use Caffeinated\Modules\Support\ServiceProvider;
+
+class AutenticacaoServiceProvider extends ServiceProvider
+{
+
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        // 'App\Model' => 'App\Policies\ModelPolicy',
+    ];
+
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        \Auth::provider('autenticacao', function () {
+            return new AutenticacaoUserProvider();
+        });
+    }
+}
---
+++ api/app/Modules/Foo/Providers/RouteServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Modules\Foo\Providers;
+
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Route;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    protected $namespace = 'App\Modules\Foo\Http\Controllers';
+
+    public function boot()
+    {
+        parent::boot();
+    }
+
+    public function map()
+    {
+        $this->mapWebRoutes();
+        $this->mapApiRoutes();
+    }
+
+    protected function mapWebRoutes()
+    {
+        Route::group([
+            'middleware' => 'web',
+            'namespace'  => $this->namespace,
+        ], function ($router) {
+            require module_path('foo', 'Routes/web.php', 'app');
+        });
+    }
+
+    protected function mapApiRoutes()
+    {
+        Route::group([
+            'middleware' => 'api',
+            'namespace'  => $this->namespace,
+            'prefix'     => 'api',
+        ], function ($router) {
+            require module_path('foo', 'Routes/api.php', 'app');
+        });
+    }
+}
