42aca3ebbabbf454ee18113810dcd1589dac2710
vinnyfs89 <viniciusfesil@gmail.com>
2019-07-25 16:29:39 -0300

    [UPDATE] Definições de estrutura de serviço do módulo de organização.
    Co-authored-by: Pedro Philipe <pedro.oliveira@basis.com.br>

--- api/app/Modules/Organizacao/Routes/api.php
+++ api/app/Modules/Organizacao/Routes/api.php
@@ -2,17 +2,8 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your module. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::get('/organizacao', function (Request $request) {
-    // return $request->organizacao();
-})->middleware('auth:api');
+Route::group([
+    'prefix' => 'organizacao'
+], function ($router) {
+    Route::apiResource('/', 'OrganizacaoController');
+});
