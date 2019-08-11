3cc81bae5da8835bc2f5c9ea09916ee3f7f33da7
vinnyfs89 <viniciusfesil@gmail.com>
2019-08-10 21:29:53 -0300

    [FIX] Correção de carregamento de rota de organização quando for números.

--- api/app/Modules/Organizacao/Routes/api.php
+++ api/app/Modules/Organizacao/Routes/api.php
@@ -3,8 +3,8 @@
 Route::group([
     'prefix' => 'organizacao'
 ], function ($router) {
+    Route::get('{co_organizacao}', 'OrganizacaoApiResourceController@show')->where('co_organizacao', '[0-9]+');;
     Route::apiResource('/', 'OrganizacaoApiResourceController');
-    Route::get('{co_organizacao}', 'OrganizacaoApiResourceController@show');
     Route::apiResource('criterio', 'CriterioApiResourceController');
     Route::apiResource('segmento', 'SegmentoApiResourceController');
 });
