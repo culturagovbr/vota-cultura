073470412b76839cf1885ec26f90edef8b44139b
vinnyfs89 <viniciusfesil@gmail.com>
2019-08-19 16:33:15 -0300

    [UPDATE] Database: Redefinição de estrutura para carregamento de dados e carga inicial para Perfil.
    Co-authored-by: Pedro <pedrophilipe.ti@gmail.com>

---
+++ database/6.carga_perfil.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.tb_perfil (co_perfil, no_perfil, ds_perfil, st_ativo) VALUES (1, 'publico', 'Público', true);
+INSERT INTO public.tb_perfil (co_perfil, no_perfil, ds_perfil, st_ativo) VALUES (2, 'eleitor', 'Eleitor', true);
+INSERT INTO public.tb_perfil (co_perfil, no_perfil, ds_perfil, st_ativo) VALUES (3, 'conselho', 'Conselho', true);
+INSERT INTO public.tb_perfil (co_perfil, no_perfil, ds_perfil, st_ativo) VALUES (4, 'organizacao', 'Organização', true);
+INSERT INTO public.tb_perfil (co_perfil, no_perfil, ds_perfil, st_ativo) VALUES (777, 'administrador', 'Administrador', true);
