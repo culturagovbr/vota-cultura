dd3941e510721e29645b05f1071a46584f97f201
guilherme.peres <guilherme.peres@basis.com.br>
2019-08-20 19:38:52 -0300

    Desabilitando DB Migration.

--- api/docker/php-fpm/docker-entrypoint.sh
+++ api/docker/php-fpm/docker-entrypoint.sh
@@ -7,8 +7,8 @@ echo "Back - Starting Endpoint of Application"
 if ! [ -d "./vendor" ]; then
     echo " Install depedences whit composer..."
     composer install --ignore-platform-reqs --verbose
-    echo "DB Migration"
-    php artisan migrate:refresh --seed
+#    echo "DB Migration"
+#    php artisan migrate:refresh --seed
 fi
 exec "$@"
 
