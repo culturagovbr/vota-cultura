0d0cc36865d2c601ce745296090418fb7e75d07f
guilherme.peres <guilherme.peres@basis.com.br>
2019-08-22 17:30:55 -0300

    Alterações para construção do webserver.

--- api/docker/php-fpm/docker-entrypoint.sh
+++ api/docker/php-fpm/docker-entrypoint.sh
@@ -6,7 +6,7 @@ echo "[ ****************** ] Starting Endpoint of Application [ ****************
 echo "Back - Starting Endpoint of Application"
 if ! [ -d "./vendor" ]; then
     echo " Install depedences whit composer..."
-    composer install --ignore-platform-reqs --verbose
+    composer dumpautoload
 #    echo "DB Migration"
 #    php artisan migrate:refresh --seed
 fi
