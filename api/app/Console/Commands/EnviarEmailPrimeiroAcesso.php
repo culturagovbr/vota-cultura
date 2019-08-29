4cdb1d5db7ffb0fc5226b1294dc4a8b190d3fbc8
Pedro Philipe Alves de Oliveira <pedro.oliveira@basis.com.br>
2019-08-29 19:16:39 -0300

    [ADD] comando para enviar emails de organizacoes e conselhos sem usuario

---
+++ api/app/Console/Commands/EnviarEmailPrimeiroAcesso.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Modules\Conselho\Model\Conselho;
+use App\Modules\Conta\Mail\Usuario\CadastroPrimeiroAcesso;
+use App\Modules\Eleitor\Model\Eleitor;
+use App\Modules\Organizacao\Model\Organizacao;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class EnviarEmailPrimeiroAcesso extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'email:primeiro-acesso';
+
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Solicitação de inscrições para primeiro acesso';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $organizacaoModel = app()->make(Organizacao::class);
+        $listaEmailsOrganizacao = $organizacaoModel
+            ->whereNull('co_usuario')->get()
+            ->pluck('representante.ds_email')
+            ->toArray();
+
+        $conselhoModel = app()->make(Conselho::class);
+        $listaEmailsConselho = $conselhoModel
+            ->whereNull('co_usuario')->get()
+            ->pluck('representante.ds_email')
+            ->toArray();
+
+        $listaEmailsGeral = array_merge(
+            $listaEmailsConselho,
+            $listaEmailsOrganizacao
+        );
+
+        foreach ($listaEmailsGeral as $email) {
+            Mail::to($email)->send(new CadastroPrimeiroAcesso());
+        }
+
+    }
+}
---
+++ api/app/Modules/Conta/Resources/Views/usuario/email/cadastro-primeiro-acesso.blade.php
@@ -0,0 +1,14 @@
+<h1>Vota Cultura</h1>
+<p>
+    Já está disponível o acesso ao Vota Cultura.
+</p>
+<p>
+    Clique <a href="{{ $linkPrimeiroAcesso }}">aqui</a> para solicitar suas credenciais.
+</p>
+
+
+<p>Atenciosamente,</p>
+<p>Minist&eacute;rio da Cidadania</p>
+
+
+(Mensagem automática, não responder)
