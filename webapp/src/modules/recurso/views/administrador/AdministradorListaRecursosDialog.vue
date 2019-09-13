491bbb560b5d0fc1e9bc5a2e85e22244d1f2b9c8
vinnyfs89 <viniciusfesil@gmail.com>
2019-09-13 15:30:40 -0300

    [UPDATE] Definição de formulário de avaliação de recurso
    Co-authored-by: Pedro Philipe <pedrophilipe.ti@gmail.com>

--- webapp/src/modules/recurso/views/administrador/AdministradorListaRecursosDialog.vue
+++ webapp/src/modules/recurso/views/administrador/AdministradorListaRecursosDialog.vue
@@ -18,10 +18,10 @@
           <v-icon>close</v-icon>
         </v-btn>
         <v-toolbar-title v-if="!!formulario.hasOwnProperty('co_recurso_inscricao')">
-          Editar Usuário
+          Editar Recurso
         </v-toolbar-title>
         <v-toolbar-title v-else>
-          Cadastrar Usuário
+          Cadastrar Recurso
         </v-toolbar-title>
         <v-spacer />
       </v-toolbar>
@@ -30,114 +30,260 @@
           <v-card>
             <v-card-text>
               <v-form
-                v-show="true"
-                ref="form"
+                ref="form_recurso"
                 v-model="valid"
                 lazy-validation
-                autocomplete="off"
               >
-                <v-container
-                  grid-list-md
-                  text-xs-center
-                >
-                  <v-layout
-                    row
-                    wrap
+                <v-layout class="mt-1">
+                  <v-flex
+                    xs12
+                    sm6
                   >
-                    <v-flex
-                      xs4
-                      md4
-                      sm12
+                    <v-radio-group
+                      v-model="formulario.co_fase"
+                      row
+                      :rules="[rules.required]"
+                      disabled
                     >
-                      <v-text-field
-                        v-model="formulario.nu_cpf"
-                        label="*CPF"
-                        append-icon="account_circle"
-                        :rules="[rules.required, rules.cpfMin, rules.cpfInvalido]"
-                        :error-messages="nomeFormularioError"
-                        mask="###.###.###-##"
-                        required
-                        :disabled="!!formulario.hasOwnProperty('co_recurso_inscricao')"
+                      <v-radio
+                        label="Conselho de cultura"
+                        value="4"
                       />
-                    </v-flex>
-                    <v-flex
-                      xs8
-                      md8
-                      sm12
-                    >
-                      <v-text-field
-                        v-model="nmPessoaFisica"
-                        :disabled="true"
-                        label="*Nome completo"
-                        append-icon="perm_identity"
-                        :error-messages="nomeFormularioError"
-                        :rules="[rules.cpfInvalido]"
-                        required
+                      <v-radio
+                        label="Organização ou entidade cultural"
+                        value="5"
                       />
-                    </v-flex>
-                    <v-flex
-                      xs6
-                      md6
-                      sm12
-                    >
-                      <v-text-field
-                        v-model="formulario.ds_email"
-                        name="ds_email"
-                        label="E-mail"
-                        validate-on-blur
-                        type="text"
-                        maxlength="80"
-                        :rules="[rules.required, rules.emailValido]"
-                      />
-                    </v-flex>
-                    <v-flex
-                      xs4
-                      md4
-                      sm12
-                    >
-                      asdasdasdasds
-                    </v-flex>
-                    <v-flex
-                      xs12
-                      sm4
-                    >
-                      <v-switch
-                        v-model="formulario.st_ativo"
-                        :label="formulario.st_ativo ? 'Ativo' : 'Inativo'"
-                        :disabled="
-                          !formulario.hasOwnProperty('co_recurso_inscricao') ||
-                            !!formulario.ds_codigo_ativacao"
-                        color="primary"
-                      />
-                    </v-flex>
-                  </v-layout>
-                </v-container>
+                    </v-radio-group>
+                  </v-flex>
+                </v-layout>
+                <v-layout
+                  wrap
+                  align-center
+                  class="mt-1"
+                >
+                  <v-flex
+                    xs12
+                    sm6
+                  >
+                    <v-text-field
+                      v-model="formulario.nu_cnpj"
+                      label="*CNPJ"
+                      append-icon="people"
+                      placeholder="99.999.999/9999-99"
+                      :error-messages="nomeConselhoError"
+                      mask="##.###.###/####-##"
+                      :rules="[rules.required, rules.cnpjMin]"
+                      required
+                      disabled
+                    />
+                  </v-flex>
+                  <v-flex
+                    xs12
+                    sm6
+                  >
+                    <v-text-field
+                      v-model="nomeConselho"
+                      label="Razão social"
+                      append-icon="people_outline"
+                      :rules="[rules.cnpjInvalido]"
+                      required
+                      style="margin-left: 20px"
+                      disabled
+                    />
+                  </v-flex>
+                </v-layout>
+                <v-layout
+                  wrap
+                  align-center
+                  class="mt-1"
+                >
+                  <v-flex
+                    xs12
+                    sm6
+                  >
+                    <v-text-field
+                      v-model="formulario.nu_cpf"
+                      label="*CPF do representante"
+                      append-icon="person"
+                      placeholder="999.999.999-99"
+                      mask="###.###.###-##"
+                      :error-messages="nomeRepresentanteError"
+                      :rules="[rules.required, rules.cpfMin]"
+                      required
+                      disabled
+                    />
+                  </v-flex>
+                  <v-flex
+                    xs12
+                    sm6
+                  >
+                    <v-text-field
+                      v-model="nomeRepresentante"
+                      label="*Nome do representante"
+                      append-icon="perm_identity"
+                      :error-messages="nomeRepresentanteError"
+                      :rules="[rules.cpfInvalido]"
+                      style="margin-left: 20px"
+                      required
+                      disabled
+                    />
+                  </v-flex>
+                </v-layout>
+
+                <v-layout
+                  wrap
+                  align-center
+                  class="mt-1"
+                >
+                  <v-flex
+                    xs12
+                    sm6
+                  >
+                    <v-text-field
+                      v-model="formulario.ds_email"
+                      data-vv-name="email"
+                      label="*E-mail do representante"
+                      append-icon="mail"
+                      placeholder="email@exemplo.com"
+                      maxlength="100"
+                      :rules="[rules.required, rules.email]"
+                      required
+                      disabled
+                    />
+                  </v-flex>
+                  <v-flex
+                    xs12
+                    sm6
+                  >
+                    <v-text-field
+                      v-model="formulario.nu_telefone"
+                      label="*Celular do representante"
+                      append-icon="phone"
+                      placeholder="(99) 99999-9999"
+                      mask="(##) #####-####"
+                      :rules="[rules.required, rules.phoneMin]"
+                      required
+                      style="margin-left: 20px"
+                      disabled
+                    />
+                  </v-flex>
+                </v-layout>
+                <v-layout
+                  wrap
+                  align-center
+                >
+                  <v-flex
+                    xs12
+                    sm12
+                  >
+                    <div class="ma-4 text-justify subheading grey--text">
+                      <b>Descrição do Recurso:</b>
+                    </div>
+                  </v-flex>
+                </v-layout>
+                <v-layout
+                  wrap
+                  align-center
+                >
+                  <v-flex
+                    xs12
+                    sm12
+                    class="ma-3"
+                  >
+                    <v-textarea
+                      v-model="formulario.ds_recurso"
+                      name="input-7-1"
+                      box
+                      solo
+                      label="Descrição do recurso"
+                      auto-grow
+                      :placeholder="'Digite seu recurso aqui.'"
+                      :counter="3000"
+                      :rules="[rules.required]"
+                      disabled
+                    />
+                  </v-flex>
+                </v-layout>
+                <v-divider></v-divider>
+
+                <v-layout
+                  wrap
+                  align-center
+                >
+                  <v-flex
+                    xs12
+                    sm12
+                    class="ma-3"
+                  >
+                    <div class="ma-4 text-justify subheading grey--text">
+                      <b>Parecer:</b>
+                    </div>
+                    <v-textarea
+                      v-model="formulario.ds_parecer"
+                      name="input-7-1"
+                      box
+                      solo
+                      label="Parecer do recurso"
+                      auto-grow
+                      :placeholder="'Digite seu recurso aqui.'"
+                      :counter="3000"
+                      :rules="[rules.required]"
+                      disabled
+                    />
+                  </v-flex>
+                </v-layout>
+                <v-layout
+                  wrap
+                  align-center
+                >
+                  <v-flex
+                    xs12
+                    sm12
+                    class="ma-3"
+                  >
+                    <v-radio-group v-model="formulario.st_parecer">
+                      <template v-slot:label>
+                        <div><strong>Avaliação</strong></div>
+                      </template>
+                      <v-radio value="1" color="success">
+                        <template v-slot:label>
+                          <div><strong class="success--text">Aceito</strong></div>
+                        </template>
+                      </v-radio>
+                      <v-radio value="0" color="error">
+                        <template v-slot:label>
+                          <div><strong class="error--text">Recusado</strong></div>
+                        </template>
+                      </v-radio>
+                    </v-radio-group>
+                  </v-flex>
+                </v-layout>
               </v-form>
             </v-card-text>
-            <v-card-actions class="justify-center">
-              <v-btn
-                @click="dialog = false"
-              >
-                <v-icon left>
-                  undo
-                </v-icon>
-                Voltar
-              </v-btn>
-              <v-btn
-                :loading="loading"
-                :disabled="!valid || loading"
-                color="primary"
-                @click.native="salvar"
-              >
-                <v-icon left>
-                  send
-                </v-icon>
-                Salvar
-              </v-btn>
-            </v-card-actions>
           </v-card>
         </v-container>
       </v-card-text>
+      <v-card-actions class="justify-center">
+        <v-btn
+          @click="dialog = false"
+        >
+          <v-icon left>
+            undo
+          </v-icon>
+          Voltar
+        </v-btn>
+        <v-btn
+          :loading="loading"
+          :disabled="!valid || loading"
+          color="primary"
+          @click.native="salvar"
+        >
+          <v-icon left>
+            send
+          </v-icon>
+          Salvar
+        </v-btn>
+      </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
@@ -246,6 +392,7 @@ export default {
         .finally(() => {
           self.loading = false;
         });
+      return true;
     },
     carregarCPF(valor) {
       this.consultarCPF(valor).then((response) => {
