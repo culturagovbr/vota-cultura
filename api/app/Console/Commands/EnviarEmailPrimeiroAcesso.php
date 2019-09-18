b3fed02174825559ced8a4be5b57122455e39d38
Pedro Philipe Alves de Oliveira <pedro.oliveira@basis.com.br>
2019-09-18 20:10:40 -0300

    [UPDATE] ajustando importações

--- api/app/Console/Commands/EnviarEmailPrimeiroAcesso.php
+++ api/app/Console/Commands/EnviarEmailPrimeiroAcesso.php
@@ -4,7 +4,6 @@ namespace App\Console\Commands;
 
 use App\Modules\Conselho\Model\Conselho;
 use App\Modules\Conta\Mail\Usuario\CadastroPrimeiroAcesso;
-use App\Modules\Eleitor\Model\Eleitor;
 use App\Modules\Organizacao\Model\Organizacao;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Mail;
