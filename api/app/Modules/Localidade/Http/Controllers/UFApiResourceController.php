8f3b63c648900e7ff41edfd415fc55d07f0b7d02
thales.martins <thales.martins@basis.com.br>
2019-09-23 18:17:07 -0300

    [ADD] teste de alteração de arquivo

--- api/app/Modules/Localidade/Http/Controllers/UFApiResourceController.php
+++ api/app/Modules/Localidade/Http/Controllers/UFApiResourceController.php
@@ -2,16 +2,26 @@
 
 namespace App\Modules\Localidade\Http\Controllers;
 
+use App\Modules\Core\Http\Controllers\AApiResourceController;
 use App\Modules\Localidade\Model\Uf;
 use App\Modules\Localidade\Service\Uf as UfService;
-use App\Modules\Core\Http\Controllers\AApiResourceController;
-use App\Modules\Organizacao\Model\Organizacao;
+use Illuminate\Http\Response;
 
 class UFApiResourceController extends AApiResourceController
 {
     public function __construct(UfService $service)
     {
         parent::__construct($service);
+    }
+
+    /** @todo teste */
+    public function index(): \Illuminate\Http\JsonResponse
+    {
+        return $this->sendResponse(
+            ['DF'],
+            "Operação Realizada com Sucesso",
+            Response::HTTP_OK
+        );
     }
 
     public function show(Uf $model): \Illuminate\Http\JsonResponse
