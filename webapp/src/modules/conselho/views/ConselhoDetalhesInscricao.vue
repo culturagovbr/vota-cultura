1a509d4ed103d8823ca0ee7707d800e8f5dca9b8
cgalvaojr <ciro.galvao@outlook.com>
2019-10-02 09:56:23 -0200

    [UPDATE] Adicionando o parecer da habilitação na tela de detalhamento da inscrição.

--- webapp/src/modules/conselho/views/ConselhoDetalhesInscricao.vue
+++ webapp/src/modules/conselho/views/ConselhoDetalhesInscricao.vue
@@ -18,53 +18,56 @@
           <v-card-text>
             <v-container>
               <conselho-detalhes-inscricao-visualizacao />
-              <v-layout>
-                <v-flex>
+              <div v-if="dadosHabilitacao">
+                <v-layout>
+                  <v-flex>
 
-                  <div class="layout">
-                    <div class="flex">
-                      <div
-                        class="v-input v-text-field v-input--is-label-active v-input--is-dirty v-input--is-disabled theme--light">
-                        <div class="v-input__control">
-                          <div class="v-input__slot">
-                            <div class="v-text-field__slot">
-                              <label aria-hidden="true"
-                                 class="v-label v-label--active v-label--is-disabled theme--light"
-                                 style="left: 0px; right: auto; position: absolute;">Resultado da habilitação</label>
+                    <div class="layout">
+                      <div class="flex">
+                        <div
+                          class="v-input v-text-field v-input--is-label-active v-input--is-dirty v-input--is-disabled theme--light">
+                          <div class="v-input__control">
+                            <div class="v-input__slot">
+                              <div class="v-text-field__slot">
+                                <label aria-hidden="true"
+                                       class="v-label v-label--active v-label--is-disabled theme--light"
+                                       style="left: 0px; right: auto; position: absolute;">Resultado da habilitação</label>
                                 <input
                                   aria-label="RG" disabled="disabled" type="text" maxlength="12"
-                                  :value="!!dadosHabilitacao.st_avaliacao ? 'Habilitado' : 'Inabilitado'"
-                                  :class="!!dadosHabilitacao.st_avaliacao ? 'color : green--text' : 'color : red--text'"
+                                  :value="!parseInt(dadosHabilitacao.st_avaliacao, 10) ? 'Habilitado' : 'Inabilitado'"
+                                  :class="!parseInt(dadosHabilitacao.st_avaliacao, 10) ? 'color : green--text' : 'color : red--text'"
                                 >
-                            </div>
-                            <div class="v-input__append-inner">
-                              <div class="v-input__icon v-input__icon--append">
-                                <i aria-hidden="true" class="v-icon v-icon--disabled material-icons theme--light">gavel</i>
+                              </div>
+                              <div class="v-input__append-inner">
+                                <div class="v-input__icon v-input__icon--append">
+                                  <i aria-hidden="true" class="v-icon v-icon--disabled material-icons theme--light">gavel</i>
+                                </div>
                               </div>
                             </div>
-                          </div>
-                          <div class="v-text-field__details">
-                            <div class="v-messages theme--light">
-                              <div class="v-messages__wrapper"></div>
+                            <div class="v-text-field__details">
+                              <div class="v-messages theme--light">
+                                <div class="v-messages__wrapper"></div>
+                              </div>
                             </div>
                           </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                </v-flex>
-              </v-layout>
-              <v-layout>
-                <v-flex>
-                  <v-text-field
-                    :value="dadosHabilitacao.ds_parecer"
-                    label="Parecer da etapa de habilitação"
-                    append-icon="subject"
-                    disabled
-                    color="red"
-                  />
-                </v-flex>
-              </v-layout>
+                  </v-flex>
+                </v-layout>
+                <v-layout>
+                  <v-flex>
+                    <v-text-field
+                      :value="dadosHabilitacao.ds_parecer"
+                      label="Parecer da etapa de habilitação"
+                      append-icon="subject"
+                      disabled
+                      color="red"
+                    />
+                  </v-flex>
+                </v-layout>
+              </div>
+
             </v-container>
           </v-card-text>
         </v-card>
