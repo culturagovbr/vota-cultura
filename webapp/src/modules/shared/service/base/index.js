99b1ad7041e4c18e43239c62b721d8bdb8e12fe7
vinnyfs89 <viniciusfesil@gmail.com>
2019-10-03 16:26:58 -0300

    [FIX] Definição de nome de arquivo ao fazer download.
    Co-authored-by: Ana Paula <anapfernandesc@gmail.com>
    Co-authored-by: Thales Farias <thalfm@gmail.com>
    Co-authored-by: Pedro <pedrophilipe.ti@gmail.com>

--- webapp/src/modules/shared/service/base/index.js
+++ webapp/src/modules/shared/service/base/index.js
@@ -71,6 +71,14 @@ export const deleteRequest = (path, id) => instance.delete(`${path}/${id}`);
 
 export const getFile = (coArquivo, config = { responseType: 'blob' }) => instance.get(`/upload/${coArquivo}`, config)
   .then((response) => {
-    const url = window.URL.createObjectURL(response.data);
-    window.open(url);
+    const { headers } = response;
+    const dadosFilename = headers['content-disposition'].split('filename=');
+    const filename = dadosFilename[1];
+  
+    const url = window.URL.createObjectURL(new Blob([response.data]));
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', filename);
+    document.body.appendChild(link);
+    link.click();
   });
