b2b9ff9b3720f2c01f0d8f7e5921272404f04875
cgalvaojr <ciro.galvao@outlook.com>
2019-10-03 14:12:25 -0200

    [UPDATE] Adicionando o resultado da habilitação da organização na tela de detalhes da inscrição.

--- webapp/src/modules/organizacao/views/OrganizacaoDetalhesInscricao.vue
+++ webapp/src/modules/organizacao/views/OrganizacaoDetalhesInscricao.vue
@@ -36,6 +36,65 @@
           <v-card-text>
             <v-container>
               <organizacao-detalhes-inscricao-visualizacao />
+              <div v-if="organizacao.organizacaoHabilitacao">
+                <v-layout>
+                  <v-flex>
+
+                    <div class="layout">
+                      <div class="flex">
+                        <div
+                          class="v-input v-text-field v-input--is-label-active v-input--is-dirty v-input--is-disabled theme--light">
+                          <div class="v-input__control">
+                            <div class="v-input__slot">
+                              <div class="v-text-field__slot">
+                                <label aria-hidden="true"
+                                       class="v-label v-label--active v-label--is-disabled theme--light"
+                                       style="left: 0px; right: auto; position: absolute;">Resultado da habilitação</label>
+                                <input
+                                  aria-label="RG" disabled="disabled" type="text" maxlength="12"
+                                  :value="organizacao.organizacaoHabilitacao.situacao_avaliacao"
+                                  :class="parseInt(organizacao.organizacaoHabilitacao.st_avaliacao, 10) === 2 ? 'color : green--text' : ''"
+                                >
+                              </div>
+                              <div class="v-input__append-inner">
+                                <div class="v-input__icon v-input__icon--append">
+                                  <i aria-hidden="true" class="v-icon v-icon--disabled material-icons theme--light">gavel</i>
+                                </div>
+                              </div>
+                            </div>
+                            <div class="v-text-field__details">
+                              <div class="v-messages theme--light">
+                                <div class="v-messages__wrapper"></div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </v-flex>
+                </v-layout>
+                <v-layout>
+                  <v-flex>
+                    <v-text-field
+                      :value="organizacao.organizacaoHabilitacao.ds_parecer"
+                      label="Parecer da etapa de habilitação"
+                      append-icon="subject"
+                      disabled
+                      color="red"
+                    />
+                  </v-flex>
+                </v-layout>
+                <v-layout>
+                  <v-flex>
+                    <v-text-field
+                      :value="organizacao.organizacaoHabilitacao.nu_nova_pontuacao"
+                      label="Pontuação após análise"
+                      append-icon="star"
+                      disabled
+                    />
+                  </v-flex>
+                </v-layout>
+              </div>
             </v-container>
           </v-card-text>
         </v-card>
@@ -45,53 +104,53 @@
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex';
-import OrganizacaoDetalhesInscricaoVisualizacao from './OrganizacaoDetalhesInscricaoVisualizacao';
+    import { mapActions, mapGetters } from 'vuex';
+    import OrganizacaoDetalhesInscricaoVisualizacao from './OrganizacaoDetalhesInscricaoVisualizacao';
 
-export default {
-  name: 'OrganizacaoDetalhesInscricao',
-  props: {
-    souAdministrador: {
-      source: {
-        type: Boolean,
-        default: false,
-      },
-    },
-  },
-  components: {
-    OrganizacaoDetalhesInscricaoVisualizacao,
-  },
-  data: () => ({
-    usuarioLogado: {},
-    organizacao: {},
-  }),
-  computed: {
-    ...mapGetters({
-      usuario: 'conta/usuario',
-      organizacaoGetter: 'organizacao/organizacao',
-    }),
-  },
-  watch: {
-    usuario(valor) {
-      this.usuarioLogado = valor;
-    },
-    usuarioLogado(usuario) {
-      if (usuario.co_organizacao) {
-        this.obterDadosOrganizacao(usuario.co_organizacao);
-      }
-    },
-    organizacaoGetter(value) {
-      this.organizacao = value;
-    },
-  },
-  methods: {
-    ...mapActions({
-      obterDadosOrganizacao: 'organizacao/obterDadosOrganizacao',
-    }),
-  },
-  mounted() {
-    this.usuarioLogado = this.usuario;
-  },
+    export default {
+        name: 'OrganizacaoDetalhesInscricao',
+        props: {
+            souAdministrador: {
+                source: {
+                    type: Boolean,
+                    default: false,
+                },
+            },
+        },
+        components: {
+            OrganizacaoDetalhesInscricaoVisualizacao,
+        },
+        data: () => ({
+            usuarioLogado: {},
+            organizacao: {},
+        }),
+        computed: {
+            ...mapGetters({
+                usuario: 'conta/usuario',
+                organizacaoGetter: 'organizacao/organizacao',
+            }),
+        },
+        watch: {
+            usuario(valor) {
+                this.usuarioLogado = valor;
+            },
+            usuarioLogado(usuario) {
+                if (usuario.co_organizacao) {
+                    this.obterDadosOrganizacao(usuario.co_organizacao);
+                }
+            },
+            organizacaoGetter(value) {
+                this.organizacao = value;
+            },
+        },
+        methods: {
+            ...mapActions({
+                obterDadosOrganizacao: 'organizacao/obterDadosOrganizacao',
+            }),
+        },
+        mounted() {
+            this.usuarioLogado = this.usuario;
+        },
 
-};
+    };
 </script>
