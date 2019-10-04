7ba6d880e02eeca366e00af43a4f3312d5cf64be
cgalvaojr <ciro.galvao@outlook.com>
2019-10-04 17:52:46 -0200

    [UPDATE] Criando o serviço de cadastrar recursos de habilitação das organizações. [UPDATE] Envio do email com a confirmação do cadastro do recurso [FIX] Removendo espaço em branco da resource ConselhoRecursoHabilitacao [FIX] Removendo o campo co_arquivo do $fillable da model ConselhoHabilitacaoRecurso

---
+++ api/app/Modules/Organizacao/Http/Controllers/OrganizacaoHabilitacaoRecursoController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Modules\Organizacao\Http\Controllers;
+
+use App\Modules\Organizacao\Service\OrganizacaoHabilitacaoRecurso;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Response;
+
+class OrganizacaoHabilitacaoRecursoController extends Controller
+{
+
+    /**
+     * @var ConselhoHabilitacaoRecurso
+     */
+    protected $service;
+
+    public function __construct(OrganizacaoHabilitacaoRecurso $service)
+    {
+        $this->service = $service;
+        $this->middleware('auth:api');
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     * @throws \App\Modules\Core\Exceptions\EParametrosInvalidos
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     */
+    public function store(Request $request)
+    {
+        return $this->sendResponse(
+            $this->service->cadastrarHabilitacaoRecurso($request->only(['dsRecurso'])),
+            'Operação realizada com sucesso',
+            Response::HTTP_CREATED
+        );
+    }
+}
