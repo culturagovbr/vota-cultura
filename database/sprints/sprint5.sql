11a12798a52efa27a11d67541a180fdb92779857
cgalvaojr <ciro.galvao@outlook.com>
2019-10-07 14:45:16 -0200

    [UPDATE] Adicionando a coluna 'dh_indicacao' ao DDL da tabela tb_conselho_indicacao

--- database/sprints/sprint5.sql
+++ database/sprints/sprint5.sql
@@ -4,6 +4,7 @@ CREATE TABLE public.tb_conselho_indicacao (
 	no_indicado text NOT NULL,
 	co_endereco integer NOT NULL,
 	co_conselho integer NOT NULL,
+	dh_indicacao timestamp NOT NULL,
 	CONSTRAINT "pk_conselho_indicacao" PRIMARY KEY (co_conselho_indicacao),
 	CONSTRAINT uk_conselho_indicacao UNIQUE (nu_cpf_indicado,co_conselho),
 	CONSTRAINT fk_conselho_indicacao_endereco FOREIGN KEY (co_endereco) REFERENCES public.tb_endereco(co_endereco)
@@ -16,3 +17,4 @@ COMMENT ON COLUMN public.tb_conselho_indicacao.nu_cpf_indicado IS 'Número do CP
 COMMENT ON COLUMN public.tb_conselho_indicacao.no_indicado IS 'Nome do indicado.';
 COMMENT ON COLUMN public.tb_conselho_indicacao.co_endereco IS 'Código referente ao endereço do indicado.';
 COMMENT ON COLUMN public.tb_conselho_indicacao.co_conselho IS 'Código do conselho.';
+COMMENT ON COLUMN public.tb_conselho_indicacao.dh_indicacao IS 'Data da indicação.';
