0707a476523c1b6da07132f336404d5ba1be1ec4
vinnyfs89 <viniciusfesil@gmail.com>
2019-10-07 14:29:01 -0300

    [UPDATE] Script de criação de tabela de histórico de habilitação de organização.
    Co-authored-by: Ana Paula <anapfernandesc@gmail.com>

---
+++ database/sprints/sprint5.sql
@@ -0,0 +1,12 @@
+CREATE TABLE public.tb_organizacao_habilitacao_historico
+(
+    co_organizacao_habilitacao_historico serial PRIMARY KEY NOT NULL,
+    co_organizacao_habilitacao int,
+    co_organizacao int NOT NULL,
+    st_avaliacao char(1) NOT NULL,
+    ds_parecer text,
+    dh_avaliacao timestamp DEFAULT current_timestamp,
+    nu_nova_pontuacao int,
+    CONSTRAINT tb_organizacao_habilitacao_historico_tb_organizacao_habilitacao_co_organizacao_habilitacao_fk FOREIGN KEY (co_organizacao_habilitacao) REFERENCES public.tb_organizacao_habilitacao (co_organizacao_habilitacao),
+    CONSTRAINT tb_organizacao_habilitacao_historico_tb_organizacao_co_organizacao_fk FOREIGN KEY (co_organizacao) REFERENCES public.tb_organizacao (co_organizacao)
+);
