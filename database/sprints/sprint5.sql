9187fd7b289bca22a31f2644c8e9ddd8f4de85cb
cgalvaojr <ciro.galvao@outlook.com>
2019-10-07 14:26:12 -0200

    [UPDATE] Adicionando o DDL da tabela tb_conselho_indicacao

---
+++ database/sprints/sprint5.sql
@@ -0,0 +1,18 @@
+CREATE TABLE public.tb_conselho_indicacao (
+	co_conselho_indicacao serial NOT NULL,
+	nu_cpf_indicado varchar(11) NOT NULL,
+	no_indicado text NOT NULL,
+	co_endereco integer NOT NULL,
+	co_conselho integer NOT NULL,
+	CONSTRAINT "pk_conselho_indicacao" PRIMARY KEY (co_conselho_indicacao),
+	CONSTRAINT uk_conselho_indicacao UNIQUE (nu_cpf_indicado,co_conselho),
+	CONSTRAINT fk_conselho_indicacao_endereco FOREIGN KEY (co_endereco) REFERENCES public.tb_endereco(co_endereco)
+);
+
+-- Column comments
+
+COMMENT ON COLUMN public.tb_conselho_indicacao.co_conselho_indicacao IS 'Chave primária da tabela.';
+COMMENT ON COLUMN public.tb_conselho_indicacao.nu_cpf_indicado IS 'Número do CPF do indicado.';
+COMMENT ON COLUMN public.tb_conselho_indicacao.no_indicado IS 'Nome do indicado.';
+COMMENT ON COLUMN public.tb_conselho_indicacao.co_endereco IS 'Código referente ao endereço do indicado.';
+COMMENT ON COLUMN public.tb_conselho_indicacao.co_conselho IS 'Código do conselho.';
