177aa680e9352fdc40e3d1a5e12aed8124ab7c1b
cgalvaojr <ciro.galvao@outlook.com>
2019-10-08 15:44:49 -0200

    [UPDATE] Adicionando a regra que valida se o conselho já cadastrou 5 indicados e não permite a inclusão de um novo.

--- api/app/Modules/Conselho/Service/ConselhoIndicacao.php
+++ api/app/Modules/Conselho/Service/ConselhoIndicacao.php
@@ -12,30 +12,58 @@ use Illuminate\Support\Facades\DB;
 
 class ConselhoIndicacao extends AbstractService
 {
+    const QTD_MAXIMO_INDICADOS = 5;
+
     public function __construct(ConselhoIndicacaoModel $model)
     {
         parent::__construct($model);
     }
 
+    /**
+     * @param array $dadosConselho
+     * @throws EParametrosInvalidos
+     */
+    public function verificarQuantidadeIndicados(array $dadosConselho)
+    {
+        $count = $this->getModel()
+            ->where($dadosConselho)
+            ->count();
+
+        if ($count === self::QTD_MAXIMO_INDICADOS) {
+            throw new EParametrosInvalidos('O conselho já atingiu o limite de indicados.');
+        }
+    }
+
+    /**
+     * @param array $verification
+     * @throws EParametrosInvalidos
+     */
+    private function verificarIndicadoCadastrado(array $verification)
+    {
+        $conselhoIndicacao = $this->getModel()
+            ->where($verification)
+            ->first();
+
+        if ($conselhoIndicacao) {
+            throw new EParametrosInvalidos(
+                'O indicado já está cadastrado.',
+                Response::HTTP_NOT_ACCEPTABLE
+            );
+        }
+    }
+
+    /**
+     * @param Collection $dados
+     * @return Model|null
+     * @throws EParametrosInvalidos
+     * @throws \HttpException
+     */
     public function cadastrar(Collection $dados): ?Model
     {
+        $this->verificarQuantidadeIndicados($dados->only(['co_conselho'])->toArray());
+        $this->verificarIndicadoCadastrado($dados->only(['co_conselho', 'nu_cpf_indicado'])->toArray());
+
         try {
-            $dadosInclusao = $dados->only([
-                'co_conselho',
-                'nu_cpf_indicado',
-            ])->toArray();
-
-            $conselhoIndicacao = $this->getModel()
-                ->where($dadosInclusao)
-                ->first();
-
-            if ($conselhoIndicacao) {
-                throw new EParametrosInvalidos(
-                    'O indicado já está cadastrado.',
-                    Response::HTTP_NOT_ACCEPTABLE
-                );
-            }
-
             $novoIndicado = parent::cadastrar($dados);
             return $novoIndicado;
         } catch (EParametrosInvalidos $queryException) {
