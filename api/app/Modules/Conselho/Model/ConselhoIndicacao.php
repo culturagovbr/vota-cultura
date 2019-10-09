5ce99396bdaa9bc79d8f07cd389666297f001e35
vinnyfs89 <viniciusfesil@gmail.com>
2019-10-08 21:09:20 -0300

    [REFACTOR] Melhorias de código
    Co-authored-by: Ciro Galvao <ciro.galvao@outlook.com>

--- api/app/Modules/Conselho/Model/ConselhoIndicacao.php
+++ api/app/Modules/Conselho/Model/ConselhoIndicacao.php
@@ -2,11 +2,14 @@
 
 namespace App\Modules\Conselho\Model;
 
+use App\Modules\Core\Exceptions\EParametrosInvalidos;
 use App\Modules\Localidade\Model\Endereco;
 use Illuminate\Database\Eloquent\Model;
 
 class ConselhoIndicacao extends Model
 {
+    const QUANTIDADE_MAXIMA_INDICADOS = 5;
+
     protected $table = 'tb_conselho_indicacao';
     protected $primaryKey = 'co_conselho_indicacao';
     protected $fillable = [
@@ -34,5 +37,39 @@ class ConselhoIndicacao extends Model
             'co_conselho',
             'co_conselho'
         );
+    }
+
+    public function quantidadeMaximaIndicadosExcedida(): bool
+    {
+        if (empty($this->co_conselho)) {
+            throw new EParametrosInvalidos('Identificador `co_conselho` não informado.');
+        }
+
+        $quantidade = $this->where($this->only('co_conselho'))->count();
+
+        if ($quantidade < self::QUANTIDADE_MAXIMA_INDICADOS) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function indicacaoJaCadastrada(): bool
+    {
+        if (empty($this->co_conselho)) {
+            throw new EParametrosInvalidos('Identificador `co_conselho` não informado.');
+        }
+
+        if (empty($this->nu_cpf_indicado)) {
+            throw new EParametrosInvalidos('Identificador `nu_cpf_indicado` não informado.');
+        }
+
+        $conselhoIndicado = $this->where($this->only('co_conselho', 'nu_cpf_indicado'))->first();
+
+        if (empty($conselhoIndicado)) {
+            return false;
+        }
+
+        return true;
     }
 }
--- api/app/Modules/Conselho/Service/ConselhoIndicacao.php
+++ api/app/Modules/Conselho/Service/ConselhoIndicacao.php
@@ -12,44 +12,31 @@ use Illuminate\Support\Facades\DB;
 
 class ConselhoIndicacao extends AbstractService
 {
-    const QTD_MAXIMO_INDICADOS = 5;
 
     public function __construct(ConselhoIndicacaoModel $model)
     {
         parent::__construct($model);
     }
 
-    private function validarQuantidadeMaximaIndicados(array $dadosConselho)
-    {
-        $count = $this->getModel()
-            ->where($dadosConselho)
-            ->count();
-
-        if ($count >= self::QTD_MAXIMO_INDICADOS) {
-            throw new EParametrosInvalidos('O conselho já atingiu o limite de indicados.');
-        }
-    }
-
-    private function verificarIndicadoCadastrado(array $verification)
-    {
-        $conselhoIndicacao = $this->getModel()
-            ->where($verification)
-            ->first();
-
-        if ($conselhoIndicacao) {
-            throw new EParametrosInvalidos(
-                'O indicado já está cadastrado.',
-                Response::HTTP_NOT_ACCEPTABLE
-            );
-        }
-    }
-
     public function cadastrar(Collection $dados): ?Model
     {
-        $this->validarQuantidadeMaximaIndicados($dados->only(['co_conselho'])->toArray());
-        $this->verificarIndicadoCadastrado($dados->only(['co_conselho', 'nu_cpf_indicado'])->toArray());
-
         try {
+            $this->getModel()->fill($dados->only(['co_conselho', 'nu_cpf_indicado']));
+            $quantidadeMaximaIndicadosExcedida = $this->getModel()->quantidadeMaximaIndicadosExcedida();
+
+            if ($quantidadeMaximaIndicadosExcedida) {
+                throw new EParametrosInvalidos('O conselho já atingiu o limite de indicados.');
+            }
+
+            $indicacaoJaCadastrada = $this->getModel()->indicacaoJaCadastrada();
+
+            if ($indicacaoJaCadastrada) {
+                throw new EParametrosInvalidos(
+                    'O indicado já está cadastrado.',
+                    Response::HTTP_NOT_ACCEPTABLE
+                );
+            }
+
             return parent::cadastrar($dados);
         } catch (EParametrosInvalidos $queryException) {
             DB::rollBack();
