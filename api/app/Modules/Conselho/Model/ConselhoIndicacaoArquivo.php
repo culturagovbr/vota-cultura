96fbab2d14cf480cbdda91fcfa57acd2309e7f18
vinnyfs89 <viniciusfesil@gmail.com>
2019-10-14 15:59:09 -0300

    [FIX] Definição correta do nome de classe.

--- api/app/Modules/Conselho/Model/ConselhoIndicacaoArquivo.php
+++ api/app/Modules/Conselho/Model/ConselhoIndicacaoArquivo.php
@@ -2,15 +2,9 @@
 
 namespace App\Modules\Representacao\Model;
 
-use App\Modules\Conselho\Model\Conselho;
-use App\Modules\Core\Helper\Telefone as TelefoneHelper;
-use App\Modules\Organizacao\Model\Organizacao;
-use App\Modules\Upload\Model\Arquivo;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\Pivot;
 
-
-class RepresentanteArquivoPivot extends Model
+class ConselhoIndicacaoArquivo extends Model
 {
     protected $table = 'rl_conselho_indicacao_arquivo';
     protected $primaryKey = 'co_conselho_indicacao_arquivo';
