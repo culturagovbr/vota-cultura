06bf8248888bf6b7e3b4d619f8fa4d70de9a0b42
Pedro Philipe Alves de Oliveira <pedro.oliveira@basis.com.br>
2019-11-18 12:04:24 -0300

    [ADD] mensagem de exceção customizada ao solicitar acesso

--- api/app/Exceptions/Handler.php
+++ api/app/Exceptions/Handler.php
@@ -3,7 +3,9 @@
 namespace App\Exceptions;
 
 use App\Modules\Core\Exceptions\EJsonException;
+use App\Modules\Core\Exceptions\EModeloNaoEncontrado;
 use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -29,9 +31,12 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        if ($exception instanceof EJsonException)
-        {
+        if ($exception instanceof EJsonException) {
             return $exception->render($request);
+        }
+
+        if ($exception instanceof ModelNotFoundException) {
+            return (new EModeloNaoEncontrado())->render($request);
         }
         return parent::render($request, $exception);
     }
